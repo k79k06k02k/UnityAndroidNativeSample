f1c8ccafdc507da44f0ea921a9703a0e3b9ccc75
arkai <k79k06k02k@gmail.com>
2019-02-10 03:04:18 +0800

    - Implemented unity link to android native

---
+++ Assets/AndroidNativeReceive.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidNativeReceive : MonoBehaviour 
+{
+    public void Receiver(string message) 
+    {
+        Debug.LogError("Receive Message: " + message);
+    }
+}
