6bc246812251919531def5df94c1c26b32923020
arkai <k79k06k02k@gmail.com>
2019-02-10 03:04:18 +0800

    - Implemented unity link to android native

---
+++ Assets/AndroidNativeSample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidNativeSample : MonoBehaviour
+{
+
+    public void OnCallActivityFuncButtonClick()
+    {
+        AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+
+        unityActivity.Call("SendMessage", "AndroidNativeReceive", "Receiver", "Hello World!");
+    }
+
+    public void OnCallClassFuncButtonClick()
+    {
+        AndroidJavaObject utility = new AndroidJavaObject("com.arkai.unityandroidnativeaample.plugin.Utility");
+        utility.Call("ShowToast", "Hello World!");
+    }
+
+}
